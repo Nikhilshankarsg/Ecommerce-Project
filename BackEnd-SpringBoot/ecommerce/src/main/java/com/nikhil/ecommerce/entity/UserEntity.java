a444f5007bc850c77d0e5f5d60a217dfd53a5b19
Nikhil27-web <nikhilshankarsg@gmail.com>
2026-01-21 09:19:14 +0530

    Small Changes in User Entity

--- BackEnd-SpringBoot/ecommerce/src/main/java/com/nikhil/ecommerce/entity/UserEntity.java
+++ BackEnd-SpringBoot/ecommerce/src/main/java/com/nikhil/ecommerce/entity/UserEntity.java
@@ -20,10 +20,10 @@ public class UserEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = false, unique = true)
+	@Column(name="email", nullable = false, unique = true)
 	private String email;
 
-	@Column(nullable = false)
+	@Column(name="password", nullable = false)
 	private String password;
 	
 	@Column(name="role")
